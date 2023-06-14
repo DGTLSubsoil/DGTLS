a26697db0f6a6559d03db33ea01a53a692f54481
DGTLSubsoil <134737751+DGTLSubsoil@users.noreply.github.com>
2023-06-15 00:16:04 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -115,9 +115,12 @@ function Header() {
                   <ul id="menu" className={menu ? 'menu active' : 'menu'}>
                     <li id="pr-1" className="menu-item-has-children" >
                       <Link href="#" onClick={toggleActive}>Home</Link>
+                      <ul className="submenu">
                         <li  >
-                          <Link href="/home3"> Home </Link>
+                          <Link href="/home3"> Home <span className="badge bg--secondary-color"> New </span>
+                          </Link>
                         </li>
+                      </ul>
                     </li>
                     <li id="pr-2" className="menu-item-has-children">
                       <Link href="#" onClick={toggleActive}>Project</Link>
