d4d45bf2244a19d39bdf0ca93b14f22322c58428
DGTLSubsoil <134737751+DGTLSubsoil@users.noreply.github.com>
2023-06-19 01:08:58 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -42,7 +42,7 @@ function Footer() {
                 </li>
                 <li className="social__item">
                   <Link href="#" className="social__link">
-                  <FontAwesomeIcon icon={faGitHub} />
+                    <FontAwesomeIcon icon={faTwitch} />
                   </Link>
                 </li>
               </ul>
