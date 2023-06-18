853be1cba30b662c178e6d0ba2d79415cfb15dba
DGTLSubsoil <134737751+DGTLSubsoil@users.noreply.github.com>
2023-06-18 18:13:54 +0200

    wrfwf

--- src/pages/api/tokenomics.js
+++ src/pages/api/tokenomics.js
@@ -3,16 +3,16 @@
 export default function handler(req, res) {
   res.status(200).json(
     {
-      "totalSupply": "15000000",
-      "publicSale": "0.24",
-      "farmingPool": "27.3",
-      "staking": "15.3",
-      "ecosystem": "0.75",
-      "advisor": "7.03",
-      "privateSale": "23.45",
-      "liquidity": "13.3",
-      "marketing": "7.3",
-      "team": "5.3"
+      "totalSupply": "1000000000",
+      "publicSale": "0.001",
+      "farmingPool": "32",
+      "staking": "20",
+      "ecosystem": "4",
+      "advisor": "3",
+      "privateSale": "15",
+      "liquidity": "15",
+      "marketing": "3",
+      "team": "8"
     },
   )
 }
