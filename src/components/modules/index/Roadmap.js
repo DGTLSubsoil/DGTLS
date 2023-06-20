0dded113e46a696469570686b9b90be56422628a
DGTLSubsoil <134737751+DGTLSubsoil@users.noreply.github.com>
2023-06-20 23:01:41 +0200

    Update Roadmap.js

--- src/components/modules/index/Roadmap.js
+++ src/components/modules/index/Roadmap.js
@@ -73,7 +73,74 @@ function Roadmap() {
                 </div>
               </div>
             </div>
-            
+            <div className="col-md-6">
+              <div
+                className="roadmap__item2 ms-auto me-md-4 aos-init aos-animate"
+                data-aos="fade-right"
+                data-aos-duration={800}
+              >
+                <div className="roadmap__item2-inner">
+                  <div className="roadmap__item2-content">
+                    <div className="roadmap__item2-header">
+                      <h4>Torkgo Farming</h4>
+                      <p>04</p>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Sunt, accusant ium! Dolores maxime numquam animi saepe
+                      sapiente tempora, veritatis velit delectus debitis
+                      provident dolore alias reiciendis cupiditate facere
+                      aliquid iusto inventore!
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 offset-md-6">
+              <div
+                className="roadmap__item2 ms-auto me-md-4 aos-init aos-animate"
+                data-aos="fade-left"
+                data-aos-duration={800}
+              >
+                <div className="roadmap__item2-inner">
+                  <div className="roadmap__item2-content">
+                    <div className="roadmap__item2-header">
+                      <h4>Stacking Program</h4>
+                      <p>05</p>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Sunt, accusant ium! Dolores maxime numquam animi saepe
+                      sapiente tempora, veritatis velit delectus debitis
+                      provident dolore alias reiciendis cupiditate facere
+                      aliquid iusto inventore!
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div
+                className="roadmap__item2 ms-auto me-md-4 aos-init aos-animate"
+                data-aos="fade-right"
+                data-aos-duration={800}
+              >
+                <div className="roadmap__item2-inner">
+                  <div className="roadmap__item2-content">
+                    <div className="roadmap__item2-header">
+                      <h4>Multichain Launchpad</h4>
+                      <p>06</p>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Sunt, accusant ium! Dolores maxime numquam animi saepe
+                      sapiente tempora, veritatis velit delectus debitis
+                      provident dolore alias reiciendis cupiditate facere
+                      aliquid iusto inventore!
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
