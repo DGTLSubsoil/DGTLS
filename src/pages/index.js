2c72b45e6aca46c541440fec051e34091232d11b
DGTLSubsoil <134737751+DGTLSubsoil@users.noreply.github.com>
2023-06-29 01:36:02 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,44 @@
-import "../../node_modules/@fortawesome/fontawesome-svg-core/styles.css";
+import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper";
 import { useEffect, useState } from "react";
 import Banner from "./../components/modules/home3/Banner";
-import Benifits from "@/components/modules/index/Benifits";
+// import Team from "@/components/modules/home3/Team";
 import Roadmap from "@/components/modules/index/Roadmap";
-import Wallet from "@/components/Wallet";
+// import Benifits from "@/components/modules/index/Benifits";
+import Token from "@/components/modules/home3/Token";
+SwiperCore.use([Autoplay, Navigation, Pagination]);
 import { fetchData } from "@/apiConfig";
+import Benifits from "@/components/modules/home3/Benifits";
+import Team from "@/components/common/Team";
+// import Team from "@/components/modules/index/Team";
 
-
-
-
-
-export default function Home() {
+const Home3 = () => {
+  const [completed, setCompleted] = useState([]);
   const [data, setData] = useState([]);
-  const [team, setTeam] = useState([]);
+  const [tokenomics, setTokenomics] = useState([]);
 
 
+  useEffect(() => {
+    const getAllData = async () => {
+      try {
+        const response = await fetchData("/completed");
+        setCompleted(response)
+      } catch (error) {
+
+      }
+    };
+    getAllData();
+  }, []);
+  useEffect(() => {
+    const getAllData = async () => {
+      try {
+        const response = await fetchData("/tokenomics");
+        setTokenomics(response)
+      } catch (error) {
+
+      }
+    };
+    getAllData();
+  }, []);
   useEffect(() => {
     const getAllData = async () => {
       try {
@@ -26,27 +50,13 @@ export default function Home() {
     };
     getAllData();
   }, []);
-
-  useEffect(() => {
-    const getAllData = async () => {
-      try {
-        const response = await fetchData("/team");
-        setTeam(response)
-      } catch (error) {
-
-      }
-    };
-    getAllData();
-  }, []);
-
   return (
     <>
       <Banner />
-      <Wallet />
       <Benifits />
+      <Token tokenomics={tokenomics} />
       <Roadmap />
     </>
   );
-}
-
-
+};
+export default Home3;
