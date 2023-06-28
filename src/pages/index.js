8cf9d3273c056c6a1f08a1d0691e4ecb0b0b4f52
DGTLSubsoil <134737751+DGTLSubsoil@users.noreply.github.com>
2023-06-29 01:21:01 +0200

    штвуч

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Benifits from "@/components/modules/index/Benifits";
 import Roadmap from "@/components/modules/index/Roadmap";
 import Wallet from "@/components/Wallet";
 import { fetchData } from "@/apiConfig";
+import Token from "@/components/modules/home3/Token";
+SwiperCore.use([Autoplay, Navigation, Pagination]);
 
 
 
@@ -32,6 +34,18 @@ export default function Home() {
         const response = await fetchData("/team");
         setTeam(response)
       } catch (error) {
+      }
+    };
+        getAllData();
+      }, []);
+      useEffect(() => {
+        const getAllData = async () => {
+          try {
+            const response = await fetchData("/notes");
+            setData(response)
+          } catch (error) {
+    
+          }
 
       }
     };
@@ -42,6 +56,7 @@ export default function Home() {
     <>
       <Banner />
       <Wallet />
+          <Token tokenomics={tokenomics} />
       <Benifits />
       <Roadmap />
     </>
